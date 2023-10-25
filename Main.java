66884fbe77e29a9cdfb9c32df437254bce72db12
REDA TARGAOUI <76877668+RedaTargaoui@users.noreply.github.com>
2023-10-25 11:15:58 +0200

    Update

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ public class Main {
 
         DBOperations dbOperations = new DBOperations();
         // Connect to database :
-        dbOperations.connection("jdbc:mysql://localhost:3306/BDD_OOL_TP1", "root", "root");
+        dbOperations.connection("jdbc:mysql://localhost:3306/database_name", "username", "password");
         // Create tables :
-        dbOperations.createTables();
+        dbOperations.createTables("SQL query to create a table");
         // Insert into tables :
-        dbOperations.insertIntoTables();
+        dbOperations.insertIntoTables("SQL query to insert into a table");
         // Delete from tables :
-        dbOperations.deleteFromTable();
+        dbOperations.deleteFromTable("SQL query to delete from a table");
         // CLose connection :
         dbOperations.closeConnection();
 
